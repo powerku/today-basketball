7cb9fcae343411912d4f7b164ccf9c36794ac747
구영준 <powerku@naver.com>
2023-05-27 01:15:44 +0900

    add: bootstrap 페이지 적용

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,10 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: [
+    // CSS file in the project
+    '~/assets/css/styles.css',
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
